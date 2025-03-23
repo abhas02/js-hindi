12959d303b9c17030113e22473b1d78d21a3b437
abhas02 <102800490+abhas02@users.noreply.github.com>
2025-03-23 20:38:14 +0530

    Add files via upload

---
+++ functionmethod.js
@@ -0,0 +1,21 @@
+const obj={
+    personName:"Abhishek",
+    greet: function(){
+        console.log(`Hello, ${this.personName}`);
+    },
+};
+setTimeout(()=> console.log('a2s'),10*1000);
+
+
+
+// console.log('HELLO FROM JS');
+// console.log('bye bye');
+
+// const a=5
+// const b=10
+
+// console.log('SUM', a+b);
+
+// setTimeout(()=>console.log('I am delayed'), 0);
+// console.log('bye bye');
+
